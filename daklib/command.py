LingmoOS/lingmo-repo-tool

commit 2e1625cfbc931bb4ce8c0930050cb0a411a7aa99

daklib/command.py: open newly created file in exclusive mode
It should be an error if the file already exists.

daklib/command.py
@@ -354,7 +354,7 @@ class CommandFile:
         filename = f"{command}.{source}_{upload.changes.version}"
         content = "OK" if command == "ACCEPT" else "NOTOK"
 
-        with open(os.path.join(upload.policy_queue.path, "COMMENTS", filename), "w") as f:
+        with open(os.path.join(upload.policy_queue.path, "COMMENTS", filename), "wx") as f:
             f.write(content + "\n")
 
     def _action_process_upload_common(self, fingerprint, section, session) -> None:

commit da2e365e05bbe44f1284a853e6574bd11867e80f

daklib/command.py: correctly open newly created file in exclusive mode
Fixes error introduced in 2e1625cfbc931bb4ce8c0930050cb0a411a7aa99

daklib/command.py
@@ -354,7 +354,7 @@ class CommandFile:
         filename = f"{command}.{source}_{upload.changes.version}"
         content = "OK" if command == "ACCEPT" else "NOTOK"
 
-        with open(os.path.join(upload.policy_queue.path, "COMMENTS", filename), "wx") as f:
+        with open(os.path.join(upload.policy_queue.path, "COMMENTS", filename), "x") as f:
             f.write(content + "\n")
 
     def _action_process_upload_common(self, fingerprint, section, session) -> None: